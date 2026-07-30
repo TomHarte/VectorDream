cb30028ecf27e974d129ed8b7bcc92392cc0b5f9
Thomas Harte <thomas.harte@gmail.com>
2026-07-29 21:30:40 -0400

    Compare entire display, albeit with incomplete painting.

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-pyz80.py -I binary/SAMDOS2 -o build/vdream.dsk src/main.z80s && open -a SimCoupe build/vdream.dsk
+pyz80.py -I binary/SAMDOS2 --mapfile build/vdream.map -o build/vdream.dsk src/main.z80s && open -a SimCoupe build/vdream.dsk
