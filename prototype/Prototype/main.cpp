c6cd24cf26f829f17f6d6d13e2c51f46fcfa3bb0
Thomas Harte <thomas.harte@gmail.com>
2026-08-10 19:59:16 -0400

    Progress integerisation.

--- prototype/Prototype/main.cpp
+++ prototype/Prototype/main.cpp
@@ -232,7 +232,15 @@ private:
 		return squares[add] - squares[sub];
 	}
 
-	uint16_t fixmul(const uint16_t a, const uint16_t b) const {
+	uint16_t ufixmul(const uint16_t a, const uint16_t b) const {
+		return (a * b) >> 8;
+	}
+
+	int16_t sfixmul(const int16_t a, const int16_t b) const {
+		return (a * b) >> 8;
+	}
+
+	int16_t hfixmul(const uint16_t a, const int16_t b) const {
 		return (a * b) >> 8;
 	}
 
@@ -339,16 +347,15 @@ private:
 		const uint8_t sin_x = uint8_t(sin(-x_rotation) * 256.0f);
 		const uint8_t cos_x = uint8_t(cos(-x_rotation) * 256.0f);
 
-		const float world_y =
-			(
-				fixmul(sin_x, src_z) -
-				fixmul(cos_x, src_y)
-			) / 256.0f;
-		const float world_z =
-			(
-				fixmul(sin_x, src_y) +
-				fixmul(cos_x, src_z)
-			) / 256.0f;
+		const int16_t wy =
+			ufixmul(sin_x, src_z) -
+			ufixmul(cos_x, src_y);
+		const int16_t wz =
+			ufixmul(sin_x, src_y) +
+			ufixmul(cos_x, src_z);
+
+		const float world_y = float(wy) / 256.0f;
+		const float world_z = float(wz) / 256.0f;
 
 		const float eye_y = (world_y / world_z) * (90.0f / field_of_view);
 		const float eye_x = player_x / (world_z * 128.0f);
